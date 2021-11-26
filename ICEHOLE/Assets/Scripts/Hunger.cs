b4019b43754b88a91f53d7a9f3da571dd8accea8
Nattatk <nattatk@live.com>
2021-11-25 20:52:04 -0700

    hi

--- ICEHOLE/Assets/Scripts/Hunger.cs
+++ ICEHOLE/Assets/Scripts/Hunger.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.UI;
+using TMPro;
 
 public class Hunger : MonoBehaviour
 {
     
-    
+    public UnityEngine.UIElements.Slider hungerSlider;
     public GameObject hi;
     public Hook hookScript;
     public int hunger;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
